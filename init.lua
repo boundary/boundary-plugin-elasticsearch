cca1849c4354d56216f9943863397ba3c7a8efab
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-06-26 17:18:57 +0000

    Code clean-up
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- init.lua
+++ init.lua
@@ -20,33 +20,31 @@ local Plugin = framework.Plugin
 local WebRequestDataSource = framework.WebRequestDataSource
 local pack = framework.util.pack
 
-local params               = framework.params
-params.pollInterval        = params.pollInterval and tonumber(params.pollInterval)*1000 or 1000
-params.name                = 'Boundary Plugin Elasticsearch'
-params.version             = '2.0'
-params.tags                = 'elasticsearch'
-local options              = url.parse(params.url)
-options.wait_for_end       = false
-local ds                   = WebRequestDataSource:new(options)
+local params = framework.params
+params.pollInterval = notEmpty(params.pollInterval, 1000)
+local options = url.parse(params.url)
+options.wait_for_end = false
+local ds = WebRequestDataSource:new(options)
 
 local plugin = Plugin:new(params, ds)
 function plugin:onParseValues(data)
-  local result = {}
   local parsed = json.parse(data)
-  -- table.insert(result, pack('ELASTIC_SEARCH_STATUS', data.status, nil))
-  table.insert(result, pack('ELASTIC_SEARCH_NODE_COUNT', parsed.nodes.count.total, nil))
-  table.insert(result, pack('ELASTIC_SEARCH_INDEX_COUNT', parsed.indices.count, nil))
-  table.insert(result, pack('ELASTIC_SEARCH_DOCUMENT_COUNT', parsed.indices.docs.count, nil))
-  table.insert(result, pack('ELASTIC_SEARCH_STORE_SIZE', parsed.indices.store.size_in_bytes, nil))
-  table.insert(result, pack('ELASTIC_SEARCH_SEGMENT_COUNT', parsed.indices.segments.count, nil))
-  table.insert(result, pack('ELASTIC_SEARCH_TOTAL_SHARDS', (parsed.indices.shards.total or 0.0), nil))
-  table.insert(result, pack('ELASTIC_SEARCH_PRIMARY_SHARDS', (parsed.indices.shards.primaries or 0.0), nil))
-  table.insert(result, pack('ELASTIC_SEARCH_FIELDDATA_MEMORY_SIZE', parsed.indices.fielddata.memory_size_in_bytes, nil))
-  table.insert(result, pack('ELASTIC_SEARCH_FIELDDATA_EVICTIONS', parsed.indices.fielddata.evictions, nil))
-  table.insert(result, pack('ELASTIC_SEARCH_FILTER_CACHE_MEMORY_SIZE', parsed.indices.filter_cache.memory_size_in_bytes, nil))
-  table.insert(result, pack('ELASTIC_SEARCH_FILTER_CACHE_EVICTIONS', parsed.indices.filter_cache.evictions, nil))
-  table.insert(result, pack('ELASTIC_SEARCH_ID_CACHE_MEMORY_SIZE', parsed.indices.id_cache.memory_size_in_bytes, nil))
-  table.insert(result, pack('ELASTIC_SEARCH_COMPLETION_SIZE', parsed.indices.completion.size_in_bytes, nil))
+
+  local result = {}
+  result['ELASTIC_SEARCH_STATUS'] = parsed.status
+  result['ELASTIC_SEARCH_NODE_COUNT'] = parsed.nodes.count.total
+  result['ELASTIC_SEARCH_INDEX_COUNT'] = parsed.indices.count
+  result['ELASTIC_SEARCH_DOCUMENT_COUNT'] = parsed.indices.docs.count
+  result['ELASTIC_SEARCH_STORE_SIZE'] = parsed.indices.store.size_in_bytes
+  result['ELASTIC_SEARCH_SEGMENT_COUNT'] = parsed.indices.segments.count
+  result['ELASTIC_SEARCH_TOTAL_SHARDS'] = parsed.indices.shards.total or 0.0
+  result['ELASTIC_SEARCH_PRIMARY_SHARDS'] = parsed.indices.shards.primaries or 0.0
+  result['ELASTIC_SEARCH_FIELDDATA_MEMORY_SIZE'] = parsed.indices.fielddata.memory_size_in_bytes
+  result['ELASTIC_SEARCH_FIELDDATA_EVICTIONS'] = parsed.indices.fielddata.evictions
+  result['ELASTIC_SEARCH_FILTER_CACHE_MEMORY_SIZE'] = parsed.indices.filter_cache.memory_size_in_bytes
+  result['ELASTIC_SEARCH_FILTER_CACHE_EVICTIONS'] = parsed.indices.filter_cache.evictions
+  result['ELASTIC_SEARCH_ID_CACHE_MEMORY_SIZE'] = parsed.indices.id_cache.memory_size_in_bytes
+  result['ELASTIC_SEARCH_COMPLETION_SIZE'] = parsed.indices.completion.size_in_bytes
   return result
 end
 
